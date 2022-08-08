d079431aa3466b47ce3c77d35c35029a641a9fe4
TheodoreBFrazier <TheodoreBFrazier>
2022-08-08 11:43:49 -0400

    Created a Home component, route and installed BrowserRouter

--- BackEnd/db/schema.sql
+++ BackEnd/db/schema.sql
@@ -6,14 +6,14 @@ CREATE DATABASE data_dev;
 --data for resume
 
 CREATE TABLE resume_data (
-job_id SERIAL PRIMARY KEY,
-job_title VARCHAR,
-date_started DATE,
-data_ended DATE,
-company_name VARCHAR,
-company_name TEXT,
-job_bullet_1 TEXT,
-job_bullet_2 TEXT,
-job_bullet_3 TEXT
+    job_id SERIAL PRIMARY KEY,
+    job_title VARCHAR,
+    date_started DATE,
+    data_ended DATE,
+    company_name VARCHAR,
+    company_name TEXT,
+    job_bullet_1 TEXT,
+    job_bullet_2 TEXT,
+    job_bullet_3 TEXT
 );
 
---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,12 @@
+function HomePage() {
+  return (
+    <div className="Home">
+      <section className="Hero-home">
+       Hello!
+      </section>
+    </div>
+  );
+}
+
+
+export default HomePage
