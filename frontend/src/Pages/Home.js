ba55608b3bc5bb9c46faaca23dff8e3899ac802d
TheodoreBFrazier <TheodoreBFrazier>
2022-08-08 12:09:37 -0400

    Home sections defined

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -2,12 +2,24 @@ function HomePage() {
   return (
     <div className="Home">
       <section className="Hero-home">
-       <h1>Hello!</h1>
-       <p>I'm Theodore.</p>
+        <h1>Hello!</h1>
+        <p>I'm Theodore.</p>
       </section>
+      <section className="intro">
+        <h1>Hi, I am an intro.</h1>
+        <p>Intro</p>
+      </section>
+      <section className="resume">
+        <h1>My Resume (?)</h1>
+      </section>
+      <section className="projects">
+        <h1>Projects will go here</h1>
+      </section>
+    <section className="Contact-me">
+        Contact form!
+    </section>
     </div>
   );
 }
 
-
-export default HomePage
+export default HomePage;
