59ae82f2d85265c32e34d076b6456d3b54436fdb
TheodoreBFrazier <TheodoreBFrazier>
2022-08-14 15:25:52 -0400

    Created a form component

---
+++ frontend/src/components/ContactForm.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      message: "",
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "text";
+    const name = target.name;
+    const message = target.message;
+
+    this.setState({
+      [name]: value,
+      [message]: value,
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Your Name:
+          <input
+            name="senderName"
+            type="text"
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <label>
+          Your Message:
+          <input
+            name="senderName"
+            type="text"
+            onChange={this.handleInputChange}
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+
+export default Contact;
