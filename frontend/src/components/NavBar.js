d00a53669b01d14c0b94c78b315b32e8d923527b
TheodoreBFrazier <TheodoreBFrazier>
2022-08-14 01:00:16 -0400

    Added a gradient border with tranparent backgroud

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,20 +2,23 @@ import { Link } from "react-router-dom";
 
 import DarkMode from "../DarkMode";
 
-
-function NavBar () {
-    return (
-        <nav className="navigation">
+function NavBar() {
+  return (
+    <nav className="navigation">
+      <ul>
+        <li>
+          <Link to="/" className="nav-logo">
+            <h3>Theo's Logo</h3>
+          </Link>
+        </li>
+        <li>
+          <div className="darkmode-toggle">
             <DarkMode />
-            <h3 className="nav-logo">
-                
-                <Link to="/">
-                    <h3>Theo's Logo</h3>
-
-                </Link>
-            </h3>
-        </nav>
-    )
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
